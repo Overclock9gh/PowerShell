6a97a4ef4b63039e77453e905ac85319bf4be672
Overclock9gh <59988450+Overclock9gh@users.noreply.github.com>
2021-02-12 16:17:17 -0500

    Add files via upload

---
+++ copier_acl.ps1
@@ -0,0 +1,31 @@
+﻿#==============================================================================#
+# Auteur: Said Hamdane
+# Date création: 29 oct. 2019
+#
+# Objectif: Script qui copie les permissions d'un fichier & qui les affiche au 
+# fur et à mesure.
+# Paramètres d'entré: Les chemins des fichiers.
+# Paramètres de sortie: Les nouvelles permissions d'un fichier.
+#==============================================================================#
+Set-Variable -ErrorAction SilentlyContinue -Name "Affirmation1" -option `
+Constant -value "Les autorisations actuelles pour le document" 
+
+Set-Variable -ErrorAction SilentlyContinue -Name "Affirmation2" -option `
+Constant -value "Les autorisations après les changements pour le document"
+
+Start-Sleep -Seconds 2
+Write-Output "$Affirmation1 modèle.txt :"
+Get-Acl -Path 'C:\tp2\modèle.txt'| Format-List AccessToString 
+
+Start-Sleep -Seconds 2
+Write-Output "$Affirmation1 test.txt :"
+Get-Acl -Path 'C:\tp2\test.txt' | Format-List AccessToString
+
+Start-Sleep -Seconds 2
+Write-Output "$Affirmation2 modèle.txt :"
+Get-Acl -Path 'C:\tp2\modèle.txt'| Format-List AccessToString
+
+Start-Sleep -Seconds 2
+Get-Acl -Path 'C:\tp2\modèle.txt' | Set-Acl -Path 'C:\tp2\test.txt'
+Write-Output "$Affirmation2 test.txt :"
+Get-Acl -Path 'C:\tp2\test.txt' | Format-List AccessToString
---
+++ start_stop_proc.ps1
@@ -0,0 +1,11 @@
+﻿#==============================================================================#
+# Auteur: Said Hamdane
+# Date création: 29 oct. 2019
+#
+# Objectif: Script qui ouvre une application & qui le referme après 5 secondes
+# Paramètres d'entré: Le chemin & le nom de l'application et le temps de sommeil. 
+# Paramètres de sortie: L'application graphique.
+#==============================================================================#
+Start-Process -FilePath C:\Windows\System32\calc.exe
+Start-Sleep -Seconds 5 
+Stop-Process -Name Calculator 
